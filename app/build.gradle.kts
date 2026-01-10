b28920dbd67f619d662bc3125ccb49ab2e1b7bd8
HyportDev <hyport.development@gmail.com>
2026-01-10 15:41:29 +0100

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.hitster"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.1.0"
+        versionCode = 4
+        versionName = "2.2.0"
 
         manifestPlaceholders.putAll(
             mapOf(
